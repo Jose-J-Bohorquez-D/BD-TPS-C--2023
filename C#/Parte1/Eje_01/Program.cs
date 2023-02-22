65638ef24ddfba83ccb1b241f556afafc03e6510
Jose Bohorquez <bd567358546@gmail.com>
2023-02-21 22:09:01 -0500

    creamos ejercicio inicial para ir subiendo todo y ir ordenando todo bien

---
+++ C#/Parte1/Eje_01/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("------------------------------------");
+Console.WriteLine("Programa que permita sumar 2 números");
+Console.WriteLine("------------------------------------");
+int n1,n2,rta;
+Console.WriteLine("Ingrese un valor a la variable n1");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese un valor a la variable n2");
+n2 = int.Parse(Console.ReadLine());
+rta = n1+n2;
+Console.WriteLine("el resultado de la suma teniendo en cuenta los valores ingresados en \n n1->" + n1 + "\ny el valor ingresado en \n n2->" + n2 + "\n la respuesta es = "+rta);
