207a43065d0dd6e78d3aab29ce4954e6d357d6d1
xxxxxx <xxx.com>
2023-02-22 21:10:06 -0500

    subimos avances en el equipo

---
+++ C#/Parte2/Eje_011/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("-------------------------------------------------------");
+Console.WriteLine("Programa que determine si una persona es mayor de edad \no no teniendo en cuenta el año actual y su año de nacimiento");
+Console.WriteLine("-------------------------------------------------------");
+Console.ReadLine();
