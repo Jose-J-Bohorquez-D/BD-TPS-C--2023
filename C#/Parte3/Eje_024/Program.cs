2bfea1e000067fceb80e7d7175e5a9512c41f723
Jose Bohorquez <bd567358546@gmail.com>
2023-02-24 19:48:03 -0500

    retomamos la gia de instruccion con la parte 3 , dejamos los ejercicios de la parte 2 pendientes pero se deben realizar y se deben tener hay listos

---
+++ C#/Parte3/Eje_024/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("----------------------------------------------------------");
+Console.WriteLine("Programa que permita determinar si una letra es o no vocal");
+Console.WriteLine("----------------------------------------------------------\n");
+string letra;
+Console.WriteLine("ingrese una letra");
+letra = Console.ReadLine();
+switch (letra)
+{
+    case "a":
+        Console.WriteLine("la letra => " + letra + "es vocal");
+    break;
+
+    case "e":
+        Console.WriteLine("la letra => " + letra + "es vocal");
+    break;
+
+    case "i":
+        Console.WriteLine("la letra => " + letra + "es vocal");
+    break;
+
+    case "o":
+        Console.WriteLine("la letra => " + letra + "es vocal");
+    break;
+
+    case "u":
+        Console.WriteLine("la letra => " + letra + "es vocal");
+    break;
+
+    default:
+    Console.WriteLine("la letra ingresada no es una vocal");
+    break;
+    
+}
+
+
---
+++ C#/Parte3/Eje_025/Program.cs
@@ -0,0 +1,25 @@
+﻿//Programa que permita realizar los siguientes requerimientos:\n1. Calcular distancia recorrida \n2. Calcular tiempo \n3. Calcular velocidad
+Console.WriteLine("----------------------------------------------------------");
+Console.WriteLine("Este Programa permite realizar las siguientes opciones:\n1. Calcular distancia recorrida \n2. Calcular tiempo \n3. Calcular velocidad");
+Console.WriteLine("----------------------------------------------------------\n");
+int distancia=0,velocidad=0,tiempo=0,rta=0,operacion=0;
+Console.WriteLine("el numero de la opercacion que desea realizar");
+operacion = int.Parse(Console.ReadLine());
+switch (operacion)
+{
+    case 1:
+        Console.WriteLine("uno");
+    break;
+
+    case 2:
+        Console.WriteLine("dos");
+    break;
+
+    case 3:
+        Console.WriteLine("tres");
+    break;
+    
+    default:
+        Console.WriteLine("el valor ingresado no corresponde a ninguna de las opciones disponibles");
+    break;
+}
