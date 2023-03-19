36d3da16c5c4d8fbad0eb407c552372fe02d5489
Jose Bohorquez <bd567358546@gmail.com>
2023-03-19 15:38:57 -0500

    terminamos el ejercicio 39 ok sigue adelante

--- C#/Parte5/Eje_039/Program.cs
+++ C#/Parte5/Eje_039/Program.cs
@@ -1,3 +1,13 @@
 ﻿Console.WriteLine("--------------------------------------------------------------------");
 Console.WriteLine("Programa que encuentre el resultado de la operación potencia donde \nel usuario ingresa el valor de la base y el exponente");
 Console.WriteLine("--------------------------------------------------------------------\n");
+    Console.WriteLine("Introduzca un nucmero de base: \n");
+    int resultado=1, i=1,bas = int.Parse(Console.ReadLine());
+    Console.WriteLine("Introduzca un nucmero de exponente: \n");
+    int exponente = int.Parse(Console.ReadLine());
+    while(i<=exponente){
+        resultado = resultado * bas;
+        i++;
+    }
+
+    Console.WriteLine("El resultado de multiplicar " + bas + " por " +exponente+ " mismo " + exponente+ " veces es igual a:" + resultado);
